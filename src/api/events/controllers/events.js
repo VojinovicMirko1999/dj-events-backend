d56af7ba045b69b17ed4248833e28e64cbcc3394
VojinovicMirko1999 <vojinovic.mirko1999@gmail.com>
2023-02-27 14:52:40 +0100

    message

---
+++ src/api/events/controllers/events.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * events controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::events.events');
+
+module.exports = {
+
+    
+}
